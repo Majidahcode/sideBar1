4001c963bc2e7ba2ec23b0e2c8dcd897b73bb69d
Majidahcode <127294905+Majidahcode@users.noreply.github.com>
2024-07-24 21:45:26 +0300

    Add files via upload

---
+++ sidebars.js
@@ -0,0 +1,17 @@
+
+
+
+
+/* global bootstrap: false */
+(() => {
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
+
+
+sidebar_small = document.querySelector("sidebar-small");
+
+sidebar_small.onclick()
